688ac27b16be7f8f35ac1718ddb96b3bf236490d
CharlesChinedum <charleschinedum2@gmail.com>
2024-02-27 13:26:08 +0100

    update: worked on implementing UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
 
-function App() {
+import logo from "../src/assets/react.svg"
 
+function App() {
+  const currentPath = window.location.href;
   return (
-    <>
-      <h1>Welcome to Ignite</h1>
-    </>
+    <main>
+      <div className="">
+        <img src={logo} alt="logo" className='logo' />
+      </div>
+      <div className="container">
+        <div className="inner-container">
+          <h2 className="header">
+            You’ve just ignited a new <span className="library">React</span> static site!
+          </h2>
+          <span className="pathname">
+            {currentPath}
+          </span>
+        </div>
+        <p className="footer">
+          Made in Nigeria with 💖
+        </p>
+      </div>
+    </main>
   )
 }
 
 export default App
+
